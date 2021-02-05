4dd858622ecb354c86e3ca6e409915296a6c1571
Dara628 <71020137+Dara628@users.noreply.github.com>
2021-02-05 14:46:10 +0000

    Update lab1ex2.java

--- lab1ex2.java
+++ lab1ex2.java
@@ -8,7 +8,7 @@ public class lab1ex2{
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter the type of the first investment: ");
         ffund = input.nextLine();
-        System.out.print("Please enter the number of the shares purshaed: how far");
+        System.out.print("Please enter the number of the shares purshaed: ");
         fnumshar = input.nextInt();
         System.out.print("Please enter the share price: : ");
         fsharp = input.nextFloat();
