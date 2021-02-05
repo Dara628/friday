b9a0d43111c144a233db609db55376163ba6c509
Dara628 <71020137+Dara628@users.noreply.github.com>
2021-02-05 14:37:03 +0000

    Febuary 5th 2:36;

---
+++ lab1ex1.java
@@ -0,0 +1,9 @@
+public class lab1ex1 {
+     public static void main(String[] args) {
+     System.out.print("c makes it easy to shoot yourself in the fooot; C++ makes it harder, byt when\nyou do, it blows away your whole leg."+
+                       "\n\n\t\t\t- Bjarne Stroustrup ");      
+} 
+        
+}
+    
+
---
+++ lab1ex2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+public class lab1ex2{
+    public static void main(String[]args){
+    String ffund, sfund;
+    int fnumshar, snumshar;
+    float fsharp, ssharp;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the type of the first investment: ");
+        ffund = input.nextLine();
+        System.out.print("Please enter the number of the shares purshaed: ");
+        fnumshar = input.nextInt();
+        System.out.print("Please enter the share price: : ");
+        fsharp = input.nextFloat();
+        
+        input.nextLine();
+        System.out.print("\n\n\nPlease enter the type of the Second investment: ");
+        sfund = input.nextLine();
+        System.out.print("Please enter the number of the shares purshaed: ");
+        snumshar = input.nextInt();
+        System.out.print("Please enter the share price: : ");
+        ssharp = input.nextFloat();    
+        System.out.println("\t\t==================================");
+        System.out.println("\t\t\tInvestment Detials");
+         System.out.println("\t\t==================================");
+         System.out.println("\nInvestment Type\t\t# Shares\tShare Price");
+         System.out.println("----------------\t--------\t-----------");
+         System.out.println(String.format("%-25s%-15d%-4.2f",ffund,fnumshar,fsharp));
+         System.out.println(String.format("%-25s%-15d%-4.2f",sfund,snumshar,ssharp));
+    }
+}
